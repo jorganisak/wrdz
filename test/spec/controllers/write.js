d6e8c8328aac5e8884db82e10b1c65c93667da60
jorganisak <jorganisak@gmail.com>
2014-03-18 23:08:51 -0400

    testing

--- test/spec/controllers/write.js
+++ test/spec/controllers/write.js
@@ -17,24 +17,28 @@ describe('Controller: WriteCtrl', function () {
   beforeEach(module('write'));
 
 
-  var scope, timeout, window, Write, ctrl;
+  var scope, $timeout, $window, Write, ctrl;
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope, _$timeout_, _$window_, _Write_) {
-    // Write = _Write_;
+    Write = _Write_;
 
-    // $timeout = _$timeout_;
-    // $window = _$window_;
+    $timeout = _$timeout_;
+    $window = _$window_;
     scope = $rootScope.$new();
     ctrl = $controller('WriteCtrl', {
       $scope: scope,
-      // Write: Write,
-      // $timeout: $timeout,
-      // $window: $window
+      Write: Write,
+      $timeout: $timeout,
+      $window: $window
 
     });
   }));
 
   it('should define medium body options', function () {
     expect(scope.mediumEditorOptionsBody).toBeDefined();
+  });
+
+  it('should define medium title options', function () {
+    expect(scope.mediumEditorOptionsTitle).toBeDefined();
   });
 });
